2fdd77885b13698841fd9b070d8ec6767630b521
rrosyidi <ravioli4stuff@gmail.com>
2022-08-11 16:55:27 -0400

    Changed margins

--- website/src/components/AboutPage/About.js
+++ website/src/components/AboutPage/About.js
@@ -5,11 +5,13 @@ class About extends Component {
   render() {
     return (
       <div className="aboutPageContainer">
-        <h5 className="myName">Hello, my name is Ravi</h5>
-        <h1 className="fullStackSentence">A <h3 className="fullStack">FullStack</h3> Software Engineer.</h1>
+        <h5 className="myName">Hello, my name is Ravi, I am</h5>
+        <h1 className="fullStackSentence">
+          A <h3 className="fullStack">FullStack</h3> Software Engineer.
+        </h1>
         <body className="body" style={{ fontWeight: "lighter" }}>
-          Currently a junior software development engineer @ Publicis Sapient. Working
-          towards building
+          Currently wokring as a junior software development engineer @ Publicis Sapient.
+          Working towards
         </body>
       </div>
     );
